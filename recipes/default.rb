efb2b6a93b9337a32f35ff69600c1a0d521cf0f8
redhatromero <redhatromero@gmail.com>
2015-12-08 09:49:07 -0500

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: basepkgs
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+
+#package 'strace'
+#package 'strace'
+
+%w[ strace tcpdump nmap mlocate ].each do |package|
+  end
